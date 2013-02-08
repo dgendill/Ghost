1ac11e96ded87d27448d8b544711aba4dbab80d3
dgendill <dgendill@gmail.com>
2013-02-07 19:56:19 -0700

    new helpers

--- Ghost.js
+++ Ghost.js
@@ -135,6 +135,50 @@ Ghost.prototype = {
         } else {
             window.clearTimeout(this.timer);
         }
+    },
+    
+    toDate : function(month) {
+        return DATES[month];    
+    },
+
+    month_ago : function(number) {
+
+        var calc = function(difference) {
+            if (Math.abs(difference) < 12) {
+                return 12 - Math.abs(difference);
+            } else {
+                return calc (difference % 12);
+            }
+        }
+    
+        var d = new Date();
+        var nowMonth = d.getMonth();
+        return calc( nowMonth - number );
+    },
+    
+    ytts : function() {
+        var toDate = this.toDate;
+        var month_ago = this.month_ago;
+        
+        $('.time').map(function() {
+            var date = $(this).text();
+            $(this).append(toDate(month_ago(date.match(/[0-9]+/)[0])));
+        });
+    },
+    
+    DATES : {
+        1 : 'jan',
+        2 : 'feb',
+        3 : 'mar',
+        4 : 'apr',
+        5 : 'may',
+        6 : 'jun',
+        7 : 'jul',
+        8 : 'aug',
+        9 : 'sep',
+        10 : 'oct',
+        11 : 'nov',
+        12 : 'dec'
     }
 };
 
@@ -147,6 +191,3 @@ var hideButtons = $('.flat-list .hide-button a');
 //var selectValue = new Action('selectIfHasValue', choices);
 var doClick = new Action('click', []);
 g.start(hideButtons, doClick);
-
-
-
